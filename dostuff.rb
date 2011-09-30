655a3ef654d8cb5ca23baaa3c7210a38bde30171
Colin Campbell-McPherson <colin@logaan.net>
2011-09-30 21:27:04 +1000

    simplified things so that info is read in once and then worked with

--- dostuff.rb
+++ dostuff.rb
@@ -4,17 +4,20 @@ require "pp"
 FileUtils.cd("repository")
 
 remote_branches =  `git branch -r`.split("\n").map(&:strip)
-non_master_branches = remote_branches - ["origin/HEAD -> origin/master", "origin/master"]
+feature_branches = remote_branches - ["origin/HEAD -> origin/master", "origin/master"]
 
-for branch in non_master_branches
-  files_changed_from_master = `git diff #{branch} --name-only`.split("\n")
+files_modified_by_each_feature_branch = {}
+for branch in feature_branches
+  files_modified_by_each_feature_branch[branch] = `git diff #{branch} --name-only`.split("\n")
+end
 
-  other_non_master_branches = non_master_branches - [branch]
-  for other_branch in other_non_master_branches
-    other_branches_files_changes_from_master = `git diff #{other_branch} --name-only`.split("\n")
-    conflicting_files = files_changed_from_master & other_branches_files_changes_from_master 
-    
-    puts "#{branch} and #{other_branch} are working on the same files" unless conflicting_files.empty?
+files_modified_by_each_feature_branch.keys.each do |branch|
+  files_changes_by_this_branch = files_modified_by_each_feature_branch.delete(branch)
+
+  for comparison_branch in files_modified_by_each_feature_branch.keys
+    files_changed_by_comparison_branch = files_modified_by_each_feature_branch[comparison_branch]
+    conflicting_files = files_changes_by_this_branch - files_changed_by_comparison_branch
+    puts "#{branch} and #{comparison_branch} are working on the same files" unless conflicting_files.empty?
   end
 end
 
